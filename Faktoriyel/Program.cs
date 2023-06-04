ccd692efeb823f46eb4e9ade338aaa1371520e87
DicleSEZER <diclesezer0@gmail.com>
2023-06-04 19:50:18 +0300

    first commit

---
+++ Faktoriyel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı girin :");
+            int sayi =Convert.ToInt32(Console.ReadLine());  
+
+
+            int faktoriyel = 1;
+            for(int i=1; i<=sayi; i++) {
+                faktoriyel *= i;
+            }
+            Console.WriteLine("Sayının faktoriyel sonucu :"+faktoriyel);
+            Console.ReadLine();
+        }
+    }
+}
